c36e586f7547d7c161cc43c08c15861ab9a05a7e
Sam Vente <savente93@proton.me>
2024-04-10 15:30:35 +0200

    add rofi-bluetooth

--- setup/arch/setup.sh
+++ setup/arch/setup.sh
@@ -81,7 +81,7 @@ function setup_creature_comforts() {
 }
 
 function config_de() {
-	paru -S dolphin nitrogen firefox flameshot i3lock i3-wm nvidia nvidia-utils polybar redshift rofi sddm ttf-firacode-nerd xss-lock brightnessctl
+	paru -S dolphin nitrogen firefox flameshot i3lock i3-wm nvidia nvidia-utils polybar redshift rofi sddm ttf-firacode-nerd xss-lock brightnessctl rofi-bluetooth-git
 	mkdir -p ~/.local/bin/rofi
 
 	# sddm theme
