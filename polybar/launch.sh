4eb99be91abef2cc42488e755eb4698ffa7227a8
Sam Vente <savente93@proton.me>
2024-04-13 09:32:41 +0000

    make polybar multi monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,4 +2,13 @@
 
 polybar-msg cmd quit
 
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar main 2>&1 | tee -a /tmp/polybar.log &
+		disown
+
+	done
+else
+	polybar main 2>&1 | tee -a /tmp/polybar.log &
+	disown
+fi
