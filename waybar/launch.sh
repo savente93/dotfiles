af57acda9e8ab1ec48d3982217181ae6f0c83f00
Sam Vente <savente93@proton.me>
2024-04-29 12:42:45 +0200

    make seperate waybar configs for different systems

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 killall waybar >/dev/null 2>&1
 
 HOSTNAME=$(hostnamectl | grep hostname | awk '{print$3}')
 
-waybar 2>&1 >/tmp/waybar.log &
+waybar -c ~/.config/waybar/$HOSTNAME.jsonc 2>&1 >/tmp/waybar.log &
 disown
