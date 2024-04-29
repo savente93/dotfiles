4456f8da155f46fed4ae23e0190140e2f453aae8
Sam Vente <savente93@proton.me>
2024-04-29 15:06:23 +0200

    fix waybar config

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -3,5 +3,5 @@ killall waybar >/dev/null 2>&1
 
 HOSTNAME=$(hostnamectl | grep hostname | awk '{print$3}')
 
-waybar -c ~/.config/waybar/$HOSTNAME.jsonc 2>&1 >/tmp/waybar.log &
+waybar -c ~/.config/waybar/$HOSTNAME.jsonc >/tmp/waybar.log 2>&1 &
 disown
