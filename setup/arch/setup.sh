d7371fcaeec395ac27f832703e208782675d924f
Sam Vente <savente93@proton.me>
2024-10-23 20:08:29 +0200

    install pixi from binary instead of aur

--- setup/arch/setup.sh
+++ setup/arch/setup.sh
@@ -43,12 +43,13 @@ function setup_dev_stuff() {
 
 	echo "setting up dev tools"
 	# tools
-	for tool in bottom cargo-audit cargo-binstall cargo-cache cargo-tarpaulin cargo-update difftastic dust eza fd git-delta gitu pixi ripgrep ruff rustup starship stylua topgrade-bin wikiman wezterm zola zoxide; do
+	for tool in bottom cargo-audit cargo-binstall cargo-cache cargo-tarpaulin cargo-update difftastic dust eza fd git-delta gitu ripgrep ruff rustup starship stylua topgrade-bin wikiman wezterm zola zoxide; do
 		if ! command -v $tool &>/dev/null; then
 			paru -S $tool --noconfirm
 		fi
 	done
 
+	curl -fsSL https://pixi.sh/install.sh | bash
 	rustup default stable
 
 	echo "setting up docker & npm"
