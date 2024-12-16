6b9de7b13d0820a60e772ce81cdb8b79b9ee78cb
Sam Vente <savente93@proton.me>
2024-12-16 08:23:07 +0100

    fix some clone issues

--- fish/functions/clone.fish
+++ fish/functions/clone.fish
@@ -6,13 +6,22 @@ function clone
     set -l SSH_URL $(op item get "Personal gitea" --fields label=ssh-url)
     set -l USERNAME $(op item get "Personal gitea" --fields label=username)
 
+
+    if test -z "$TOKEN" | test -z "$BASE_URL" | test -z "$SSH_URL" | test -z "$USERNAME"
+        echo "Could not retrieve secrets"
+        return
+    end
+
     set SELECTED_REPO $(curl -Ls "$BASE_URL/api/v1/users/$USERNAME/repos" -H "accept: application/json"  -H "Authorization: token $TOKEN" | jq -r "map(.name)|.[]" | fzf)
+
     if test -z "$SELECTED_REPO"
         echo "no repo selected"
         return
     end
-    set REPO_TOPIC $(curl -Ls "$BASE_URL/api/v1/repos/$USER_NAME/$SELECTED_REPO/topics" -H "accept: application/json"  -H "Authorization: token $TOKEN" | jq -r ".topics[0]")
-    if test -z "$REPO_TOPIC"
+
+    set REPO_TOPIC $(curl -Ls "$BASE_URL/api/v1/repos/$USERNAME/$SELECTED_REPO/topics" -H "accept: application/json"  -H "Authorization: token $TOKEN" | jq -r ".topics[0] // empty")
+
+    if [ -z "$REPO_TOPIC" ]
         echo "repo had no topic"
         return
     end
