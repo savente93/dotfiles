be358734b80e6effb576d924d3ecbe98d43c9487
Sam Vente <savente93@proton.me>
2024-12-31 14:30:06 +0100

    fix(fish): grep expression in wifi function

--- fish/functions/wifi.fish
+++ fish/functions/wifi.fish
@@ -1,4 +1,4 @@
 
-function wifi()
-    nmcli d wifi connect -a "$(nmcli -f SSID d wifi list | sort | uniq | grep -v SSID | xargs | fzf --header "Please select a wifi network")"
+function wifi
+    nmcli d wifi connect -a "$(nmcli -f SSID d wifi list | sort | uniq | grep -v -F -e 'SSID' -e '--' | string trim | fzf --header "Please select a wifi network")"
 end
