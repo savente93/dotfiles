08201cb96062fe57d3ea0400c10593efa6f0cb54
Sam Vente <savente93@proton.me>
2025-01-05 16:17:18 +0100

    add script to recreate bin branch in helix fork

---
+++ helix/recreate-bin.sh
@@ -0,0 +1,12 @@
+git checkout master
+git branch -D bin
+git checkout -b bin
+git pull https://github.com/MattCheely/helix labels-for-config-menus --no-rebase
+git pull https://github.com/DylanBulfin/helix dylanbulfin/bufferline_duplicate_names --no-rebase
+git pull https://github.com/plul/helix goto_next_diag_workspace --no-rebase
+git pull https://github.com/NikitaRevenco/helix feat/different-case-modes --no-rebase
+git pull https://github.com/ElSargo/helix master --no-rebase
+git pull https://github.com/CedricMeu/helix better-bufferline --no-rebase
+
+
+
