094388119c4c37e90db6f0b826e23ee856bf20c9
Sam Vente <savente93@proton.me>
2025-01-20 09:05:43 +0100

    fix clone.fish

--- .config/fish/functions/clone.fish
+++ .config/fish/functions/clone.fish
@@ -11,21 +11,21 @@ function clone
         return
     end
 
-    set SELECTED_REPO $(curl -Ls "$BASE_URL/api/v1/users/$USERNAME/repos" -H "accept: application/json"  -H "Authorization: token $TOKEN" | jq -r "map(.name)|.[]" | fzf)
+    set SELECTED_REPO $(curl -Ls "https://$BASE_URL/api/v1/users/$USERNAME/repos" -H "accept: application/json"  -H "Authorization: token $TOKEN" | jq -r "map(.name)|.[]" | fzf)
 
     if test -z "$SELECTED_REPO"
         echo "no repo selected"
         return
     end
 
-    set REPO_TOPIC $(curl -Ls "$BASE_URL/api/v1/repos/$USERNAME/$SELECTED_REPO/topics" -H "accept: application/json"  -H "Authorization: token $TOKEN" | jq -r ".topics[0] // empty")
+    set REPO_TOPIC $(curl -Ls "https://$BASE_URL/api/v1/repos/$USERNAME/$SELECTED_REPO/topics" -H "accept: application/json"  -H "Authorization: token $TOKEN" | jq -r ".topics[0] // empty")
 
     if [ -z "$REPO_TOPIC" ]
         echo "repo had no topic"
         return
     end
 
-    mkdir -p "$HOME/Documents/projects/$REPO_TOPIC"
-    git clone "git@$BASE_URL:$USERNAME/$SELECTED_REPO.git" "$HOME/Documents/projects/$REPO_TOPIC/$SELECTED_REPO"
+    mkdir -p "$HOME/projects/$REPO_TOPIC"
+    git clone "git@$BASE_URL:$USERNAME/$SELECTED_REPO.git" "$HOME/projects/$REPO_TOPIC/$SELECTED_REPO"
 
 end
