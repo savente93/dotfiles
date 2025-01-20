556c957115c083f7d53da0f0771ae8aad53e5fe8
Sam Vente <savente93@proton.me>
2025-01-20 13:13:06 +0100

    install calc lib during setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -251,7 +251,7 @@ function setup_fonts() {
 function setup_de() {
 	sudo rm /etc/sddm.conf
 	sudo ln -s ~/dotfiles/sddm.conf /etc/sddm.conf
-	install_tools paru brightnessctl cronie gammastep grim sddm sddm-catppuccin-git slurp swappy swaybg swayidle swaylock waybar webp-pixbuf-loader xdg-desktop-portal xdg-desktop-portal thunar xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal-wlr walker
+	install_tools paru brightnessctl cronie gammastep grim sddm sddm-catppuccin-git slurp swappy swaybg swayidle swaylock waybar webp-pixbuf-loader xdg-desktop-portal xdg-desktop-portal thunar xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal-wlr walker libqalculate
 	mkdir -p ~/Wallpapers
 	curl -Ls https://raw.githubusercontent.com/gh0stzk/dotfiles/master/config/bspwm/rices/andrea/walls/wall-01.webp -o ~/Wallpapers/wall.webp
 	curl -Ls https://wallpapercave.com/wp/wp2639448.png -o ~/Wallpapers/locked.png
