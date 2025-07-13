27a592c0275c665ff170d34c268b11f7fbb201a4
Sam Vente <savente93@proton.me>
2025-07-13 11:35:42 +0200

    feat(nvim): add healthcheck binding foor zola.nvim

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -3,6 +3,11 @@ return {
     dir = '~/projects/lua/zola.nvim',
     config = function()
       require 'zola'
+
+      vim.keymap.set('n', '<leader>zh', function()
+        require 'zola'
+        vim.cmd [[checkhealth zola]]
+      end)
     end,
   },
 }
