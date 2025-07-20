c58aa2911919f9b287ebff1bb86368d3933f89e8
Sam Vente <savente93@proton.me>
2025-07-20 13:34:02 +0200

    feat: add treesitter-textojbects

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,8 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  lazy = false,
+  branch = 'master',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
@@ -18,6 +20,7 @@ return { -- Highlight, edit, and navigate code
       'query',
       'vim',
       'vimdoc',
+      'rust',
     },
 
     -- Autoinstall languages that are not installed
@@ -27,6 +30,41 @@ return { -- Highlight, edit, and navigate code
     },
     indent = { enable = true },
     textobjects = {
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']f'] = '@function.outer',
+          [']]'] = { query = '@class.outer', desc = 'Next class start' },
+          [']o'] = { query = { '@loop.inner', '@loop.outer' } },
+
+          -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+          -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+          [']s'] = { query = '@local.scope', query_group = 'locals', desc = 'Next scope' },
+          [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+        -- Below will go to either the start or the end, whichever is closer.
+        -- Use if you want more granular movements
+        -- Make it even more gradual by adding multiple queries and regex.
+        goto_next = {
+          [']d'] = '@conditional.outer',
+        },
+        goto_previous = {
+          ['[d'] = '@conditional.outer',
+        },
+      },
       select = {
         enable = true,
 
