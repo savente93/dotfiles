34e51b341aeae645b9575ddb982e51d95ef21f1d
Sam Vente <savente93@proton.me>
2025-07-21 12:10:44 +0200

    fix: update treesitter-textoject bindings

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -35,34 +35,20 @@ return { -- Highlight, edit, and navigate code
         set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
           [']f'] = '@function.outer',
-          [']]'] = { query = '@class.outer', desc = 'Next class start' },
-          [']o'] = { query = { '@loop.inner', '@loop.outer' } },
-
-          -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-          -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-          [']s'] = { query = '@local.scope', query_group = 'locals', desc = 'Next scope' },
+          [']c'] = '@class.outer',
           [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
         },
         goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
+          [']F'] = '@function.outer',
+          [']C'] = '@class.outer',
         },
         goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
+          ['[f'] = '@function.outer',
+          ['[c'] = '@class.outer',
         },
         goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-        -- Below will go to either the start or the end, whichever is closer.
-        -- Use if you want more granular movements
-        -- Make it even more gradual by adding multiple queries and regex.
-        goto_next = {
-          [']o'] = '@conditional.outer',
-        },
-        goto_previous = {
-          ['[o'] = '@conditional.outer',
+          ['[F'] = '@function.outer',
+          ['[C'] = '@class.outer',
         },
       },
       select = {
@@ -80,7 +66,6 @@ return { -- Highlight, edit, and navigate code
           -- nvim_buf_set_keymap) which plugins like which-key display
           ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
           -- You can also use captures from other query groups like `locals.scm`
-          ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
         },
 
         selection_modes = {
